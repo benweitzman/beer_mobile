c203eeaecdf4b8eb56751107f518f2563f2fb642
Ben Weitzman <benjamin.weitzman@tufts.edu>
2013-01-11 15:00:28 -0800

    Initial Commit

---
+++ beerTests/beerTests.h
@@ -0,0 +1,13 @@
+//
+//  beerTests.h
+//  beerTests
+//
+//  Created by Ben Weitzman on 1/11/13.
+//  Copyright (c) 2013 Ben Weitzman. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface beerTests : SenTestCase
+
+@end
---
+++ beerTests/beerTests.m
@@ -0,0 +1,32 @@
+//
+//  beerTests.m
+//  beerTests
+//
+//  Created by Ben Weitzman on 1/11/13.
+//  Copyright (c) 2013 Ben Weitzman. All rights reserved.
+//
+
+#import "beerTests.h"
+
+@implementation beerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in beerTests");
+}
+
+@end
