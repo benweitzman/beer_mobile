fa35ebe4c98ea1deb44210f3ad1abdd0029e041e
Ben Weitzman <benjamin.weitzman@tufts.edu>
2013-01-11 20:09:10 -0800

    started work on mobile app

---
+++ beer/BeerViewController.m
@@ -0,0 +1,38 @@
+//
+//  BeerViewController.m
+//  beer
+//
+//  Created by Ben Weitzman on 1/11/13.
+//  Copyright (c) 2013 Ben Weitzman. All rights reserved.
+//
+
+#import "BeerViewController.h"
+
+@interface BeerViewController ()
+
+@end
+
+@implementation BeerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ beer/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  beer
+//
+//  Created by Ben Weitzman on 1/11/13.
+//  Copyright (c) 2013 Ben Weitzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
